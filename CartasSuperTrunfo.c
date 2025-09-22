1b8b3fd41421b1a60509ae43a3a8777931e45156
luismarcelo806 <t626431@gmail.com>
2025-09-22 23:01:22 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,18 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Carta 1
+  char estado1;
+  char codigo1[4];
+  char nomecidade[50];
+  int populacao1;
+  float area1;
+  float pibi1;
+  int pontosturisticos1;
+
+  //carta 2
+  char estado1;
+
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
